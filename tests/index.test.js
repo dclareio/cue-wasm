5dd9b599787aab7cc72714cf675867d43376bc6c
keeganmccallum <keeganmccallum3@gmail.com>
2022-06-16 11:30:33 -0700

    allow passing list of cue strings

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,8 +5,19 @@ test: string
 test: "test"
 `;
 
+const cueString2 = `
+hello: string
+hello: "world"
+`;
+
 test('parses cue to js object', async () => {
   const cue = await CUE.init();
   const result = cue.parse(cueString1);
   expect(result).toEqual({ test: "test" });
+});
+
+test('parses list of cue strings to js object', async () => {
+  const cue = await CUE.init();
+  const result = cue.parse([cueString1, cueString2]);
+  expect(result).toEqual({ test: "test", hello: "world" });
 });
